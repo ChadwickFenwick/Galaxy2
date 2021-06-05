148836af6408b5617da853c00cf0cff4219d53a3
ChadwickFenwick <chadwickfenwick@gmail.com>
2021-06-05 13:05:31 -0700

    Sound, Music, Stuff!

--- user_actions.py
+++ user_actions.py
@@ -1,3 +1,6 @@
+from kivy.uix.relativelayout import RelativeLayout
+
+
 def keyboard_closed(self):
     self.keyboard.unbind(on_key_down=self.on_keyboard_down)
     self.keyboard.unbind(on_key_up=self.on_keyboard_up)
@@ -5,9 +8,9 @@ def keyboard_closed(self):
 
 def on_keyboard_down(self, keyboard, keycode, text, modifiers):
     if keycode[1] == 'left':
-        self.current_speed_x += self.SPEED_X  # Move Left
+        self.current_speed_x = +self.SPEED_X  # Move Left
     elif keycode[1] == 'right':
-        self.current_speed_x -= self.SPEED_X  # Move Right
+        self.current_speed_x = -self.SPEED_X  # Move Right
     return True
 
 
@@ -17,10 +20,12 @@ def on_keyboard_up(self, keyboard, keycode):
 
 
 def on_touch_down(self, touch):
-    if touch.x < self.width / 2:
-        self.current_speed_x += self.SPEED_X  # Move Left
-    else:
-        self.current_speed_x -= self.SPEED_X  # Move Right
+    if not self.state_game_over and self.state_game_has_started:
+        if touch.x < self.width / 2:
+            self.current_speed_x += self.SPEED_X  # Move Left
+        else:
+            self.current_speed_x -= self.SPEED_X  # Move Right
+    return super(RelativeLayout, self).on_touch_down(touch)
 
 
 def on_touch_up(self, touch):
